62d7ca98fef8966e6ebe47408de1f2fe3883ed04
Joe Yates <joe.g.yates@gmail.com>
2022-10-03 18:11:39 +0200

    Implement a Logger

--- index.js
+++ index.js
@@ -10,6 +10,71 @@ const shuffle = array => {
   return shuffled
 }
 
+class Logger {
+  constructor() {
+    this.level = Logger.INFO
+  }
+
+  moreVerbose() {
+    if (this.level >= Logger.MOST_VERBOSE) {
+      return false
+    }
+    this.level++
+    return true
+  }
+
+  lessVerbose() {
+    if (this.level <= Logger.LEAST_VERBOSE) {
+      return false
+    }
+    this.level--
+    return true
+  }
+
+  debug(...args) {
+    if (this.level < Logger.DEBUG) {
+      return
+    }
+    console.log((new Date()).toJSON(), ...args)
+  }
+
+  info(...args) {
+    if (this.level < Logger.INFO) {
+      return
+    }
+    console.log(...args)
+  }
+
+  warn(...args) {
+    if (this.level < Logger.WARN) {
+      return
+    }
+    console.log(...args)
+  }
+
+  error(...args) {
+    if (this.level < Logger.ERROR) {
+      return
+    }
+    console.log(...args)
+  }
+
+  fatal(...args) {
+    if (this.level < Logger.FATAL) {
+      return
+    }
+    console.log(...args)
+  }
+}
+
+Logger.FATAL = 0
+Logger.ERROR = 1
+Logger.WARN = 2
+Logger.INFO = 3
+Logger.DEBUG = 4
+Logger.MOST_VERBOSE = Logger.DEBUG
+Logger.LEAST_VERBOSE = Logger.FATAL
+
 class PhotoSlideshow {
   constructor(window, viewer) {
     this.window = window

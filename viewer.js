1495c1e661f107adf706294f0b0dfe85273b7e7d
Joe Yates <joe.g.yates@gmail.com>
2022-10-04 15:24:01 +0200

    Separate 3 states: preloading, ready and showing

--- viewer.js
+++ viewer.js
@@ -1,21 +1,78 @@
 import EventEmitter from './event-emitter.js'
 
+const CAPTION_HEIGHT = 32
+
+class ImageDetails {
+  constructor(image, index) {
+    this.image = image
+    this.index = index
+    this.img = null
+  }
+
+  load(success, failure) {
+    this.img = new Image()
+    this.img.onload = success
+    this.img.onerror = failure
+    this.img.src = this.image.url
+  }
+}
+
 class Viewer extends EventEmitter {
   constructor(parent, logger) {
     super()
     this.parent = parent
+    this.logger = logger
     this.caption = null
     this.img = null
-    this.preloadedReady = false
     this.preloader = null
-    this.preloadImage = null
-    this.preloadIndex = null
+    this.ready = null
     this.showCaption = false
-    this.showIndex = null
+    this.showing = null
+  }
+
+  get nextIndex() {
+    if (this.preloader) {
+      return this.preloader.index + 1
+    } else {
+      if (this.showing) {
+        if (this.showing.index === this.ready.index) {
+          return this.showing.index + 1
+        } else {
+          // The ready image hasn't been shown
+          return this.ready.index
+        }
+      } else {
+        // Nothing has been shown yet
+        if (this.ready) {
+          return this.ready.index
+        } else {
+          // preload not yet called
+          return 0
+        }
+      }
+    }
+  }
+
+  get previousIndex() {
+    if (this.preloader) {
+      return this.preloader.index - 1
+    } else {
+      if (this.ready) {
+        return this.ready.index - 1
+      } else {
+        return -1
+      }
+    }
+  }
+
+  get showIndex() {
+    if (!this.showing) {
+      return null
+    }
+    return this.showing.index
   }
 
   start() {
-    this.preloader = new Image()
     this.caption = document.createElement('h1')
     this.caption.classList.add('caption')
     this.parent.append(this.caption)
@@ -25,40 +82,45 @@ class Viewer extends EventEmitter {
   }
 
   preload(image, index) {
-    this.preloadedReady = false
-    this.preloadImage = image
-    this.preloadIndex = index
-    this.preloader.src = image.url
-    this.preloader.onload = this.imageLoaded.bind(this)
-    this.preloader.onerror = this.imageFailed.bind(this)
+    this.preloader = new ImageDetails(image, index)
+    this.preloader.load(this.imageLoaded.bind(this), this.imageFailed.bind(this))
   }
 
   cancelPreload() {
-    this.preloader.onload = null
-    this.preloader.onerror = null
+    this.preloader = null
   }
 
   imageLoaded() {
-    this.preloadedReady = true
-    this.emit('imageLoaded')
+    if (!this.preloader) {
+      return
+    }
+    this.ready = this.preloader
+    this.emit('imageLoaded', this.preloader.image, this.preloader.index)
+    this.preloader = null
   }
 
   imageFailed() {
-    this.preloadedReady = false
-    this.emit('imageFailed')
+    this.emit('imageFailed', this.preloader.image, this.preloader.index)
+    this.preloader = null
   }
 
-  show() {
-    if (!this.preloadedReady) {
+  showPreloaded() {
+    if (!this.ready) {
       return
     }
+    this.logger.debug(`Showing preloaded image ${this.ready.index} '${this.ready.image.url}'`)
+    this.showing = this.ready
+    this.resize()
+  }
+
+  resize() {
     const captionHeight = this.showCaption ? CAPTION_HEIGHT : 0
     const viewport = this.parent.getBoundingClientRect()
     const viewportWidth = viewport.right - viewport.left - 16
     const viewportHeight = viewport.bottom - viewport.top - (16 + captionHeight)
     const viewportProportions = viewportHeight / viewportWidth
-    const imageWidth = this.preloadImage.width
-    const imageHeight = this.preloadImage.height
+    const imageWidth = this.showing.image.width
+    const imageHeight = this.showing.image.height
     const imageProportions = imageHeight / imageWidth
     let width, height
     let left, top
@@ -78,7 +140,7 @@ class Viewer extends EventEmitter {
       top = `${(viewportHeight - height) / 2}px`
     }
     this.img.style.visibility = 'hidden'
-    this.img.src = this.preloadImage.url
+    this.img.src = this.showing.image.url
     this.img.width = width
     this.img.height = height
     this.img.style.left = left
@@ -86,11 +148,10 @@ class Viewer extends EventEmitter {
     this.img.style.visibility = 'visible'
     if (this.showCaption) {
       this.caption.style.visibility = 'visible'
-      this.caption.innerHTML = this.preloadImage.url
+      this.caption.innerHTML = this.showing.src
     } else {
       this.caption.style.visibility = 'hidden'
     }
-    this.showIndex = this.preloadIndex
   }
 }
 

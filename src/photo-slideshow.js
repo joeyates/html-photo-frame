e37ca04bdedcd3b582c4f5a9d984369a5a252583
Joe Yates <joe.g.yates@gmail.com>
2022-10-11 10:29:01 +0200

    Remove redundant logging

--- src/photo-slideshow.js
+++ src/photo-slideshow.js
@@ -369,7 +369,6 @@ class PhotoSlideshow {
 
   showPreloaded() {
     this.showNextTimeout = null
-    this.logger.debug(`Showing ${this.viewer.ready.image.url}`)
     this.viewer.showPreloaded()
     this.previousShow = new Date
     this.next()
